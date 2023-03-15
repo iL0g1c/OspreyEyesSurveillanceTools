bf86a21e3289cca3109265aff9dbc476a9d96206
iL0g1c <mathtodd05@gmail.com>
2023-03-15 16:18:35 -0400

    set ospreyEyes for release

--- src/ospreyEyes.py
+++ src/ospreyEyes.py
@@ -11,7 +11,7 @@ from chat import parseChat
 
 intents = discord.Intents.all()
 load_dotenv()
-BOT_TOKEN = os.getenv("DISCORD_TOKEN_BETA")
+BOT_TOKEN = os.getenv("DISCORD_TOKEN")
 geofs_session_id = os.getenv("GEOFS_SESSION_ID")
 bot = commands.Bot(intents=intents, command_prefix="beta! ")
 CATALOG_DIR = "catalog/"
