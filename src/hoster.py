98c5979db77737227e1e6f6eeaf8ac08d25e14f4
iL0g1c <mathtodd05@gmail.com>
2023-03-18 10:15:41 -0400

    added untested code for hoster

--- src/hoster.py
+++ src/hoster.py
@@ -1,28 +1,23 @@
 import time
 import os
 from dotenv import load_dotenv
+import asyncio
 from map_api import get_users
-from catalog import Catalog
+from callsigns import parseCallsigns
 from mutiplayer_api import init_server_instance, sendMsg
 
-def setup():
-	load_dotenv()
-	geofs_session_id = os.getenv("GEOFS_SESSION_ID")
-	if not os.path.exists("callsigns.jsonl"):
-		with open("callsigns.jsonl", "w") as fp:
-			pass
-	return geofs_session_id
+load_dotenv()
+geofs_session_id = os.getenv("GEOFS_SESSION_ID")
 
-def main():
-	geofs_session_id = setup()
+async def main():
 	myId = init_server_instance(geofs_session_id)
-	catalog = Catalog()
+
 	print("Starting Tracking...")
 	while True:
 		users = get_users()[1]
-		catalog.parse(users)
-		if catalog.msg != "":
-			myId = sendMsg(myId, catalog.msg, geofs_session_id)
+		msg = await parseCallsigns(users)
+		if msg != "":
+			myId = sendMsg(myId, msg, geofs_session_id)
 		time.sleep(1)
 if __name__ in "__main__":
-	main()
+	asyncio.run(main())
